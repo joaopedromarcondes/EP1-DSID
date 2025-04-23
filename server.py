2c3f34a14571782a02d0c7336bda2ebea4c6ed27
clebenjr <clebenjuniorcgarcia@hotmail.com>
2025-04-23 15:52:47 -0300

    adicionando operação void e long no proto e no server

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 from comunicacao_grpc import mensagens_pb2, mensagens_pb2_grpc
 import grpc
 from concurrent import futures
+from random import randint
 
 class MensagemServicer(mensagens_pb2_grpc.MensagemServicer):
 
     def RecebePessoa(self, request, context):
         print(request)
-        return mensagens_pb2.Pessoa(nome="andré")
+        return mensagens_pb2.MensagemPessoa(nome="andré")
+    
+    def RecebeVoid(self, request, context):
+        print("Recebendo Void")
+
+    def RecebeLong(self, request, context):
+        print(request)
+        var = randint(1, 1000)
+        valor *= var
+        print(valor)
+        return mensagens_pb2.MensagemLong(valor)
+   
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
