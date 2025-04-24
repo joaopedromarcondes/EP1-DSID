7d006c3a06f68ef41362952f81ac51534925f81e
Joao Pedro Marcondes <joaopopm@usp.br>
2025-04-24 17:26:09 -0300

    Cliente está funcionando igual ao json

--- grpc_client.py
+++ grpc_client.py
@@ -4,29 +4,6 @@ import grpc
 import time
 from random import randint
 
-def calcula_desempenho(tempos):
-    res = dict()
-    soma = 0
-    for i in range(10):
-        soma += tempos[i]
-    res['media'] = soma / 10
-    variancia = 0
-    for i in range(10):
-        variancia += (tempos[i] - (soma / 10)) ** 2
-
-    res['variancia'] = variancia / 10
-    res['desvio_padrao'] = variancia ** 0.5
-    return res
-
-def print_desempenho(tempos, nome):
-    res = calcula_desempenho(tempos)
-    print(nome)
-    print(f"Média: {res['media']}")
-    print(f"Variância: {res['variancia']}")
-    print(f"Desvio Padrão: {res['desvio_padrao']}\n")
-    return res
-
-
 
 def client():
     print("Começando Cliente....\n")
@@ -41,8 +18,6 @@ def client():
         stub.FuncaoLong(mensagens_pb2.MensagemLong(valor=val))
         fim = time.perf_counter()
         tempos_Long.append(fim - inicio)
-        print(inicio)
-    print_desempenho(tempos_Long, "Função Long")
     res["Long"] = tempos_Long
     
 
@@ -53,8 +28,42 @@ def client():
         stub.FuncaoVoid(Empty())
         fim = time.perf_counter()
         tempos_Void.append(fim - inicio)
-    print_desempenho(tempos_Void, "Função Void")
     res["Void"] = tempos_Void
+
+
+    tempos_VariosLong = []
+    for i in range(10):
+        val = [randint(0, 2**50) for _ in range(8)]
+        inicio = time.perf_counter()
+        stub.FuncaoVariosLong(mensagens_pb2.MensagemVariosLong(valores=val))
+        fim = time.perf_counter()
+        tempos_VariosLong.append(fim - inicio)
+    res["VariosLong"] = tempos_VariosLong
+
+    tempos_String = []
+    for i in range(10):
+        val = "a" * (2 ** i)
+        inicio = time.perf_counter()
+        stub.FuncaoString(mensagens_pb2.MensagemString(valor=val))
+        fim = time.perf_counter()
+        tempos_String.append(fim - inicio)
+    res["String"] = tempos_String
+
+    tempos_Complexa = []
+    for i in range(10):
+        val = "a" * (2 ** i)
+        inicio = time.perf_counter()
+        stub.FuncaoComplexa(mensagens_pb2.MensagemComplexa(id=1,
+        nome="João",
+        ativo=True,
+        salario=5000.0,
+        estadoCivil="Casado",
+        filhos=["Ana", "Pedro"],
+        cargo="Engenheiro",
+        idade=30))
+        fim = time.perf_counter()
+        tempos_Complexa.append(fim - inicio)
+    res["Complexa"] = tempos_Complexa
 
     return res
 
